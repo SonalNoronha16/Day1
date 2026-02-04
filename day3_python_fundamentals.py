310295e94c19037b91f887ab9796732d341931b9
SonalNoronha16 <noronhasonal04@gmail.com>
2026-02-04 10:47:24 +0530

    day3_pythonfundamentals

--- day3_python_fundamentals.py
+++ day3_python_fundamentals.py
@@ -1,4 +1,4 @@
-"""
+
 #List examples
 numbers=[10,20,30,40]
 #Tuple example
@@ -11,21 +11,18 @@ print(a[-3:-1])
 print(a[1:4:3])
 print(a[2:9:2])
 print(a[-4:-1:2])
-"""
-"""
+
 #change
 marks=[85,72,90]
 marks.append(88)
 marks.pop()
 marks.sort()
 print(marks)
-"""
-"""
+
 marks=[85,72,90]
 marks.reverse()
 print(marks)
-"""
-"""
+
 #The Inventory Manager
 inventory = ["Apples", "Bananas", "Carrots", "Dates"]
 
@@ -38,8 +35,7 @@ inventory.remove("Bananas")
 inventory.sort()
 
 print("Final Updated Inventory:", inventory)
-"""
-"""
+
 #The Data Slicer
 temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
 
@@ -51,15 +47,14 @@ print("Afternoon Peak Readings:", afternoon_peak)
 
 last_three_hours = temperatures[-3:]
 print("Last 3 Hours Readings:", last_three_hours)
-"""
-"""
+
 #The Immutable Config
 screen_res = (1920, 1080)
 
 print("Current Resolution:", str(screen_res[0]) + "x" + str(screen_res[1]))
 
 screen_res[0] = 1280 
-"""
+
 #The Fix
 screen_res = (1920, 1080)
 
