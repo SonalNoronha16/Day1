3ec6aacf7e9732c03a62bca95e179687661c7f28
SonalNoronha16 <noronhasonal04@gmail.com>
2026-02-04 10:42:43 +0530

    day_3_python_fundamentals

--- day3_python_fundamentals.py
+++ day3_python_fundamentals.py
@@ -20,6 +20,49 @@ marks.pop()
 marks.sort()
 print(marks)
 """
+"""
 marks=[85,72,90]
 marks.reverse()
 print(marks)
+"""
+"""
+#The Inventory Manager
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+
+print("Current Inventory:", inventory)
+
+inventory.append("Eggs")
+
+inventory.remove("Bananas")
+
+inventory.sort()
+
+print("Final Updated Inventory:", inventory)
+"""
+"""
+#The Data Slicer
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+
+print("First Reading:", temperatures[0])
+print("Last Reading:", temperatures[-1])
+
+afternoon_peak = temperatures[3:6]
+print("Afternoon Peak Readings:", afternoon_peak)
+
+last_three_hours = temperatures[-3:]
+print("Last 3 Hours Readings:", last_three_hours)
+"""
+"""
+#The Immutable Config
+screen_res = (1920, 1080)
+
+print("Current Resolution:", str(screen_res[0]) + "x" + str(screen_res[1]))
+
+screen_res[0] = 1280 
+"""
+#The Fix
+screen_res = (1920, 1080)
+
+print("Current Resolution:", str(screen_res[0]) + "x" + str(screen_res[1]))
+
+print("Tuples cannot be modified!")
