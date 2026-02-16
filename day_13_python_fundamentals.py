c2656cf34e57038d431f1602e5e87963c92b37f8
SonalNoronha16 <noronhasonal04@gmail.com>
2026-02-16 17:02:31 +0530

    day13_python_fundamentals

---
+++ day_13_python_fundamentals.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+df = pd.read_csv(r"D:\DS_AI_Internship\src\students.csv")
+
+df.head()
+df.tail()
+df.shape
+df.info()
+df.describe()
+
+
+#Exploratory data analysis
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = {
+    "Age": [25,30,35,40,28,32,45,50,23,36,29,41],
+    "Salary": [30000,40000,50000,65000,42000,48000,80000,90000,28000,52000,46000,70000],
+    "Experience": [1,3,7,10,2,5,15,20,1,8,4,12],
+    "Department": ["IT","HR","IT","Finance","HR","IT","Finance","Finance","HR","IT","HR","Finance"],
+    "Gender": ["M","F","M","M","F","F","M","M","F","F","M","F"]
+}
+
+
